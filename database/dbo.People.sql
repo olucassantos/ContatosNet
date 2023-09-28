a3d37aa264b9ee34cbb44f1e64220fe8574037d0
SN1087637 <SN1087637@ESN990D1151238>
2023-09-27 21:55:25 -0300

    Adicionar arquivos de projeto.

---
+++ database/dbo.People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[People] (
+    [ContactID] INT            NOT NULL IDENTITY(1,1),
+    [Name]      NVARCHAR (100) NULL,
+    [Company]   NVARCHAR (100) NULL,
+    [Telephone] NVARCHAR (20)  NULL,
+    [Email]     NVARCHAR (255) NULL,
+    [Client]    BIT            NULL,
+    [LastCall]  DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([ContactID] ASC)
+);
